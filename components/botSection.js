48cb347c98800e471549d5af2b2d5fe3c0bbc54a
Sakshamag21 <107304333+Sakshamag21@users.noreply.github.com>
2022-06-26 10:51:32 +0530

    Update botSection.js

--- components/botSection.js
+++ components/botSection.js
@@ -3,7 +3,7 @@ import tr1 from "../data1.json"
 function BotSection(){
 return(
     tr1.map(record1=>{
-    return(<div className={styles.botContainer}>
+    return(<div className={styles.botContainer} key={record1.id}>
         <img className={styles.imgh} src={record1.imgh}></img>
         <div className={styles.desc}>
             <p className={styles.nameFont}><b>{record1.name}</b></p>
