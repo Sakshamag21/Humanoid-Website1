0144f312e68bea775e7c9b4adfb90a2885cb463a
Sakshamag21 <107304333+Sakshamag21@users.noreply.github.com>
2022-06-26 10:55:50 +0530

    Update software.js

--- components/software.js
+++ components/software.js
@@ -4,7 +4,7 @@ import tr3 from "../data2.json";
 function SoftwareCard(){
 return(tr3.map(record3=>{
     return(
-        <div className={styles.containerImage}>
+        <div className={styles.containerImage} key={record3.id}>
             <img className={styles.iconSoftware} src={record3.image}></img>
             <div className={styles.nameIconBack}></div>
             <div className={styles.nameIcon}>{record3.name}</div>
